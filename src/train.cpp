3b557b4cbcccd5c8f15b434c31b6ddae7955ff81
Kotyara21 <73665937+Kotyara21@users.noreply.github.com>
2024-05-12 21:00:07 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,12 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
+#include "train.h"
+
+int main() {
+  Train train;
+  train.addCage(true);
+  train.addCage(false);
+  train.addCage(true);
+  std::cout << "Lenght of the train: " << train.getLenght() << std::endl;
+  return 0;
+}
